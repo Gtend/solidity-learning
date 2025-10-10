4ea20510e3215fbf1cf5036ff7474d8249ea3533
Gtend <86248339+Gtend@users.noreply.github.com>
2025-10-10 21:21:01 +0900

    hardhat test

---
+++ test/hardhat_test.ts
@@ -0,0 +1,68 @@
+import hre from "hardhat";
+import { ethers } from "ethers";
+
+// hardhat test
+// describe("hardhat-test", async () => {
+describe("hardhat-test", () => {
+  it("hardhat ethers test", async () => {
+    // console.log(hre.ethers);
+    // const signers = await hre.ethers.getSigners();
+    // console.log(signers);
+    // console.log(signers.length);
+
+    // bob -> alice : 100 ETH
+    const signers = await hre.ethers.getSigners();
+    const bobWallet = signers[0];
+    const aliceWallet = signers[1];
+
+    // transaction 만들기
+    const tx = {
+      from: bobWallet.address,
+      to: aliceWallet.address,
+      // 1 ETH = 10^18 wei
+      // 100 ETH = 100 * 10^18 wei
+      value: hre.ethers.parseEther("100"), // wei
+    };
+
+    // transaction 보내기 (원래는 signature(bob의 private key로 만들 수. 있음) 받아야함.)
+    const txHash = await bobWallet.sendTransaction(tx);
+    const receipt = await txHash.wait();
+    // console.log(await hre.ethers.provider.getTransaction(txHash.hash));
+    // console.log("--------------------");
+    // console.log(receipt);
+  });
+
+  it("ethers test", async () => {
+    const provider = new ethers.JsonRpcProvider("http://localhost:8545");
+
+    const bobWallet = new ethers.Wallet(
+      "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+      provider
+    );
+    const aliceWallet = new ethers.Wallet(
+      "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"
+    );
+
+    // Transaction 만들기
+    const tx = {
+      from: bobWallet.address,
+      to: aliceWallet.address,
+      value: ethers.parseEther("100"),
+      // chainId: 31337,
+    };
+
+    const populatedTx = await bobWallet.populateTransaction(tx);
+    //console.log(populatedTx);
+
+    // Signature
+    const signedTx = await bobWallet.signTransaction(populatedTx);
+    const txHash = await provider.send("eth_sendRawTransaction", [signedTx]);
+    console.log(txHash);
+    console.log(
+      ethers.formatEther(await provider.getBalance(bobWallet.address))
+    );
+    console.log(
+      ethers.formatEther(await provider.getBalance(aliceWallet.address))
+    );
+  });
+});
